dbc2a90bd493a886e0e2e21fa50389a77bd22dbc
hari <harinath.b.07@gmail.com>
2022-08-27 02:23:17 +0530

    Css added

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, session
 import cv2
 import mediapipe as mp
 import tensorflow as tf
 from helper import *
-
 
 app = Flask(__name__)
 
@@ -30,13 +29,20 @@ def gen_frames():  # generate frame by frame from camera
             if len(sequence) == sequence_length:
                 res = model.predict(np.expand_dims(sequence, axis=0))[0]
                 if res[np.argmax(res)] > threshold: 
-                    sentence.append(actions[np.argmax(res)])
-            if len(sentence) > 1: 
-                sentence = sentence[-1:]
-##                idx = pageData.index("</body>")
-##                tmp = pageData[:idx] + f"<h3>{sentence}</h3>" + pageData[idx:]
-##                pageData = tmp
-            cv2.putText(image,'hello'.join(sentence), (3,85), 
+                    tmp = actions[np.argmax(res)]
+                    if(tmp == 'Idle'):
+                        sentence = [tmp]
+                    if(len(sentence) > 0):
+                        if(not(sentence[-1] == tmp)):
+                            sentence.append(tmp)
+                    else:
+                        sentence.append(tmp)
+                    if(len(sentence) > 1 and sentence[-2] == 'Idle' and tmp != 'Idle'):
+                            del sentence[-2]             
+                if len(sentence) > 3: 
+                    sentence = sentence[-1:]
+                    
+            cv2.putText(image,' '.join(sentence), (3,85), 
                        cv2.FONT_HERSHEY_SIMPLEX, 1, (52,232,235), 2, cv2.LINE_AA)
             ret,buffer=cv2.imencode('.jpg', image)
             frame = buffer.tobytes()
